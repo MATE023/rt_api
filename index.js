2be1f11eb13d7220204237d2d9a2c94f5b477c19
teoslivka@gmail.com <teoslivka@gmail.com>
2024-08-14 12:40:25 -0700

    seconf commit

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-const express = require("express"); const app = express(); app.get("/", (req, res) => { res.send("Express on Vercel"); }); const PORT = process.env.PORT || 5000; app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
+//const express = require("express"); const app = express(); app.get("/", (req, res) => { res.send("Express on Vercel"); }); const PORT = process.env.PORT || 5000; app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
+//module.exports = app;
+
+const express = require('express');
+const app = express();
+const problemRoutes = require('./src/routes/problemRoutes.js');
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(express.json());
+app.use('/', problemRoutes);
+
+app.get('/', (req, res) => {
+    res.send("API");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+
 module.exports = app;
---
+++ src/routes/problemRoutes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+
+const problems = ["problem"];
+
+// Get all users
+router.get('/problems', async (req, res) => {
+  res.json(problems);
+});
+
+router.post('/problems', (req, res) => {
+    const problem = req.body;
+    problems.push(problem);
+    res.status(201).json(problems);
+})
+
+router.put('/problems/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedProblem = req.body;
+    problems[id] = updatedProblem;
+    res.json(updatedProblem);
+})
+
+router.delete('/users/:id', (req, res) => {
+    const id = req.params.id;
+    problems.splice(id, 1);
+    res.status(204).send();
+})
+/*
+const getProblems = (): Problem[] => {
+    var da = new Date();
+    return [
+        { id: 1, title: 'Two Sum', num: 1, description: 'Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.', difficulty: 'easy', url: new URL('https://leetcode.com/problems/two-sum'), topics: ["hashmap, arrays"], createdAt:  da.getDate(), subQuestions: []},
+        { id: 2, title: 'Add Two Numbers', num: 2, description: 'You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list', difficulty: 'medium', url: new URL("https://leetcode.com/problems/add-two-numbers"), topics: ["arrays"], createdAt:  da.getDate(), subQuestions: [] },
+        { id: 3, title: 'Longest Substring Without Repeating Characters', num: 3, description: 'Given a string s, find the length of the longest substring without repeating characters', difficulty: 'medium', url: new URL("https://leetcode.com/problems/longest-substring-without-repeating-characters"), topics: ["strings, arrays"], createdAt:  da.getDate(), subQuestions: [] }
+    ]; 
+}
+*/
+
+module.exports = router;
