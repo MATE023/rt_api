589df29cea007c2bfc6a3c2ca5f1317af1ba95c8
teoslivka@gmail.com <teoslivka@gmail.com>
2024-09-11 00:05:15 -0400

    add example descrpiton

--- src/entities/Problem.js
+++ src/entities/Problem.js
@@ -1,7 +1,7 @@
 const problem = {
     id: '1',
     title: 'two sum',
-    description: 'description',
+    description: 'Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.',
     difficulty: 'easy',
     url: 'https://leetcode.com/problems/two-sum/description/',
     solutionIds: ['1', '2']
--- src/entities/Question.js
+++ src/entities/Question.js
@@ -1,7 +1,11 @@
+const a = require('./../entities/Answer');
+const answers = a.answers;
+
 const question1 = {
     id: '1',
     content: 'What data structure should we use?',
     answerChoiceIds: ['1', '2', '3', '4'],
+    answerChoices: [answers[0], answers[1], answers[2], answers[3]],
     correctAnswer: '1'
 }
 
@@ -9,6 +13,7 @@ const question2 = {
     id: '2',
     content: 'What should the keys and values of the hashmap be?',
     answerChoiceIds: ['5', '6', '7', '8'],
+    answerChoices: [answers[4], answers[5], answers[6], answers[7]],
     correctAnswer: '6'
 }
 
@@ -16,6 +21,7 @@ const question3 = {
     id: '3',
     content: 'What should you search for in the hashmap?',
     answerChoiceIds: ['9', '10', '11', '12'],
+    answerChoices: [answers[8], answers[9], answers[10], answers[11]],
     correctAnswer: '11'
 }
 
@@ -23,35 +29,40 @@ const question4 = {
     id: '4',
     content: 'What is the access time of the hashmap?',
     answerChoiceIds: ['13', '14', '15', '16'],
+    answerChoices: [answers[12], answers[13], answers[14], answers[15]],
     correctAnswer: '16'
 }
 
 const question5 = {
     id: '5',
     content: 'What is the answer2?',
-    answerChoiceIds: ['1', '2', '3', '4'],
-    correctAnswer: '2'
+    answerChoiceIds: ['13', '14', '15', '16'],
+    answerChoices: [answers[12], answers[13], answers[14], answers[15]],
+    correctAnswer: '16'
 }
 
 const question6 = {
     id: '6',
     content: 'What is the answer2?',
-    answerChoiceIds: ['1', '2', '3', '4'],
-    correctAnswer: '1'
+    answerChoiceIds: ['13', '14', '15', '16'],
+    answerChoices: [answers[12], answers[13], answers[14], answers[15]],
+    correctAnswer: '16'
 }
 
 const question7 = {
     id: '7',
     content: 'What is the answer2?',
-    answerChoiceIds: ['1', '2', '3', '4'],
-    correctAnswer: '4'
+    answerChoiceIds: ['13', '14', '15', '16'],
+    answerChoices: [answers[12], answers[13], answers[14], answers[15]],
+    correctAnswer: '16'
 }
 
 const question8 = {
     id: '8',
     content: 'What is the answer2?',
-    answerChoiceIds: ['1', '2', '3', '4'],
-    correctAnswer: '2'
+    answerChoiceIds: ['13', '14', '15', '16'],
+    answerChoices: [answers[12], answers[13], answers[14], answers[15]],
+    correctAnswer: '16'
 }
 
 module.exports.questions = [question1, question2, question3, question4, question5, question6, question7, question8];
