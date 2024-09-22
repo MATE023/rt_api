97ee0b7bec0ae45d990c26dd3bb50c689c6e8cc7
teoslivka@gmail.com <teoslivka@gmail.com>
2024-09-22 16:33:36 -0400

    add new problem

--- src/entities/Problem.js
+++ src/entities/Problem.js
@@ -1,4 +1,4 @@
-const problem = {
+const problem1 = {
     id: '1',
     title: 'two sum',
     description: 'Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.',
@@ -7,4 +7,13 @@ const problem = {
     solutionIds: ['1', '2']
 };
 
-module.exports.problems = [problem];
+const problem2 = {
+    id: '2',
+    title: 'add two numbers',
+    description: 'You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list. You may assume the two numbers do not contain any leading zero, except the number 0 itself.',
+    difficulty: 'medium',
+    url: 'https://leetcode.com/problems/add-two-numbers/description/',
+    solutionIds: ['1', '2']
+};
+
+module.exports.problems = [problem1, problem2];
