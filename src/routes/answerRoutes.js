4adb9cb611fcbf6e9450ebcf7f55223c74bc4dfe
MATEOPC\wllsl <teoslivka@gmail.com>
2025-05-03 11:50:38 -0400

    fixes answers route

--- src/routes/answerRoutes.js
+++ src/routes/answerRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const a = require('./../entities/Answer');
 const answers = a.answers;
+const q = require('./../entities/Question');
+const questions = q.questions;
 
-//answers: id, content
+//answers: id, content, questionId
 // Get all answers
 router.get('/answers', async (req, res) => {
   res.json(answers);
@@ -17,6 +19,16 @@ router.get('/answers/:id', async (req, res) => {
             res.json(element);
         }
     })
+});
+
+router.get('/answers/question/:id', async (req, res) => {
+    currAnswers = []
+    answers.forEach(ans => {
+        if(req.params.id == ans.questionId) {
+            currAnswers.push(ans);
+        }
+    })
+    res.json(currAnswers);
 });
 
 module.exports = router;
